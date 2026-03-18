bf1c89b3963f7eaef84baf925d2e93d025eef002
Francesco Marchione <fmarchione.code@gmail.com>
2026-03-18 18:16:52 +0100

    feat: aggiunge search_in_category per filtrare spese per mese e categoria

--- functions.py
+++ functions.py
@@ -1,9 +1,11 @@
 from datetime import datetime,timedelta
+from unicodedata import category
+#creazione di un dizionario con le categorie e le causali associate, oltre al limite di spesa per ogni categoria
 
 categorie = {
     "BENZINA":{
         "causali" : ["benzina","benza"],
-        "limite" : 140
+        "limite" : 160
     },
     "STIPENDIO":{
         "causali" : ["stipendio"],
@@ -11,7 +13,7 @@ categorie = {
     },
     "CIBO":{
         "causali": ["pizza","sushi","colazione","merenda","gelato","piadina","poke","kfc","mangiare","focaccia","bar","caffe","brioches","tiramisu","frappe","birra","drink","snack"],
-        "limite": 110
+        "limite": 100
     },
     "CRYPTO/INVESTIMENTI":{
         "causali": ["crypto","pepe","eth"],
@@ -27,11 +29,11 @@ categorie = {
     },
     "CURA_PERSONALE":{
         "causali":["capelli","medicine","dentifricio","colluttorio","testine","scovolini"],
-        "limite":100
+        "limite":70
     },
     "REGALI":{
         "causali":["regalo","san valentino"],
-        "limite":100
+        "limite":75
     },
     "ABBIGLIAMENTO":{
         "causali":["VESTITI","INTIMO"],
@@ -47,7 +49,8 @@ categorie = {
     }
 }
 
-def trova_categoria(a:str):
+def trova_categoria(a:str):#funzione che prende in input una causale e restituisce la categoria a cui appartiene, se non appartiene a nessuna categoria restituisce "ALTRO"
+
     a = str(a).lower()#devi mettere DATAFRAME + COLONNA CAUSALE
     for x,y in categorie.items():
         for elemento in y["causali"]:
@@ -55,12 +58,14 @@ def trova_categoria(a:str):
                 return x
     return "ALTRO"
 
+#funzione che prende in input la somma per mese e categoria e stampa un alert se la spesa supera il limite
 
 def alert_category(somma_mese):
-
     for x,y in somma_mese.items():
         if y > categorie[x[1]]["limite"]:
             print( f"\n-ALERT|{x[1]}|{x[0]}\n hai speso:{y}$ | limite:{categorie[x[1]]['limite']}$\n")
+
+#funzione che prende in input il dataframe pulito e predice la spesa per il prossimo mese basandosi sulla media degli ultimi 3 mesi
 
 def predict_next_month(data_frame):
     date_limit = data_frame["DATA"].max() - timedelta(days=90)
@@ -75,18 +80,19 @@ def predict_next_month(data_frame):
                 
 
             print(f"\n-CATEGORIA |{x}\n Prediction {abs(y)}$ | Limite:{categorie[x]['limite']}$ | {stato}\n")
-            
 
+#funzione che prende in input il mese e la categoria e stampa tutte le causali associate a quella categoria per quel mese
+def search_in_category(df_pulito, file_mese, category):
+    # Filtro per mese
+    df_mese = df_pulito[df_pulito["MESE"] == file_mese]
+    # Filtro anche per categoria
+    df_filtrato = df_mese[df_mese["CATEGORIA"] == category]
 
-
-
-
-
-
-
-
-
-
+    # Ora cicla sul DataFrame filtrato, non sul dizionario
+    for indice, riga in df_filtrato.iterrows():
+        print(f"\n-CAUSALE | {riga['CAUSALE']} | {riga['USCITA']}$")
+    print(f"\n-----------------------------------------\n-TOTALE | {df_filtrato['USCITA'].sum()}$\n-----------------------------------------\n")
+   
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 
 import pandas as pd
 import functions as f
-from functions import predict_next_month, alert_category, trova_categoria
+from functions import predict_next_month, alert_category, trova_categoria, search_in_category
+
+#carica il file excel e lo salva in una variabile, in questo modo possiamo accedere a tutte le sue sheet
 
 df = pd.ExcelFile("ROUTINE.xlsx")
-foglio_corrente = pd.read_excel("ROUTINE.xlsx",sheet_name="BILANCIO DICEMBRE 25",header=2)
+
+#carica la sheet "BILANCIO DICEMBRE 25" e la salva in una variabile, in questo modo possiamo accedere a tutte le sue colonne
+
+foglio_corrente = pd.read_excel("ROUTINE.xlsx",
+sheet_name="BILANCIO DICEMBRE 25",header=2)
+
+#crea una lista vuota che conterrà tutte le tabelle da modificare, in questo modo possiamo unire tutte le tabelle in un unico dataframe
+
+
 df_da_modificare = []
+
+#ciclo che scorre tutte le sheet del file excel e salva in una variabile tutte le tabelle da modificare, in questo modo possiamo unire tutte le tabelle in un unico dataframe
 
 for sheet in df.sheet_names:
     if "BILANCIO" in sheet:
@@ -18,15 +30,41 @@ for sheet in df.sheet_names:
         for tabella in tabella_1,tabella_2,tabella_3:
             df_da_modificare.append(tabella)
 
+#creazione di un dizionario con le categorie e le causali associate, oltre al limite di spesa per ogni categoria
+
 df_unito = pd.concat(df_da_modificare,ignore_index=True)
+
+#creazione di un dizionario con le categorie e le causali associate, oltre al limite di spesa per ogni categoria
+
 df_unito = df_unito.loc[:, ~df_unito.columns.str.contains("Unnamed")]
+
+#creazione di un dizionario con le categorie e le causali associate, oltre al limite di spesa per ogni categoria
+
 df_pulito = df_unito.dropna(subset =['CAUSALE']).copy()
+
+#creazione di un dizionario con le categorie e le causali associate, oltre al limite di spesa per ogni categoria
+
 df_pulito["ENTRATA"] = pd.to_numeric(df_pulito["ENTRATA"],errors="coerce")
+
+#creazione di un dizionario con le categorie e le causali associate, oltre al limite di spesa per ogni categoria
+
 df_pulito["USCITA"] = pd.to_numeric(df_pulito["USCITA"],errors="coerce")
+
+#creazione di un dizionario con le categorie e le causali associate, oltre al limite di spesa per ogni categoria
+
 df_pulito["IMPORTO"] =  df_pulito["ENTRATA"].fillna(0) - df_pulito["USCITA"].fillna(0)
 df_pulito["CATEGORIA"] = df_pulito["CAUSALE"].apply(f.trova_categoria)
+
+#creazione di un dizionario con le categorie e le causali associate, oltre al limite di spesa per ogni categoria
+
 df_pulito["DATA"] = pd.to_datetime(df_pulito["DATA"],errors="coerce")
+
+#creazione di un dizionario con le categorie e le causali associate, oltre al limite di spesa per ogni categoria
+
 somma_mese = df_pulito.groupby(["MESE","CATEGORIA"])["IMPORTO"].sum()
+
+#creazione di un dizionario con le categorie e le causali associate, oltre al limite di spesa per ogni categoria
+
 alert = df_pulito.groupby(["MESE","CATEGORIA"])["IMPORTO"].sum().unstack().fillna(0)
 
 
@@ -34,6 +72,8 @@ alert = df_pulito.groupby(["MESE","CATEGORIA"])["IMPORTO"].sum().unstack().filln
 
 if __name__ =="__main__":
     #alert_category(somma_mese)
-    print(predict_next_month(df_pulito))
-
-
+    #print(alert_category(somma_mese))
+    #print(predict_next_month(df_pulito))
+    #print(somma_mese)
+    search_in_category(df_pulito, "BILANCIO FEBBRAIO 26", "BENZINA")
+    
